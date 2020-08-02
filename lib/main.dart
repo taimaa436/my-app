8d2e93265ea17cde447f7448536c515f136cd8c2
Elbez Hammouda <hammoudae@gmail.com>
2020-08-02 16:52:57 +0200

    Update 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'model.dart';
-import 'article-screen.dart';
 import 'log-in.dart';
 
 
